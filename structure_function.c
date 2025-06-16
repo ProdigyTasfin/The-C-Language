2d8ae842600b731c3d68ea8e158897a33990aa4d
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-06-16 22:51:03 +0600

    Initial commit of my VS Code project

--- structure_function.c
+++ structure_function.c
@@ -19,7 +19,7 @@ int main()
     struct employee e1; //employee
     e1.age = 43; //age
     strcpy(e1.name, "Tas Fin"); //copy name from string 
-    e1.salary = 50432.22; 
+    e1.salary = 50432.22; //salary in float
     show(e1);
     return 0;
 }
