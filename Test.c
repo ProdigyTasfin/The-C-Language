2f3886beab99a5c98e7c83edfbc3126ed460fbd3
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-06-29 01:14:54 +0600

    Initial commit of my VS Code project

--- Test.c
+++ Test.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 int main(){
-    printf("My Family Information-\n\tName: Devid\n\tFather: Daud\n\tMother: Stella\n\nEducation Information:\n\tSchool: Saint Joseph School\n\tCollege: Mirpur College\n\tUniversity: East West University");
-return 0;
+
+    // LAB - 02
+
+    // int height, width, length;
+    // printf("Enter Your Height: ");
+    // scanf("%d", &height);
+    // printf("Enter Your Width: ");
+    // scanf("%d", &width);
+    // printf("Enter Your Length: ");
+    // scanf("%d", &length);
+    
+    // printf(" Your Volume is: %d", height * width * length);
+
+
+    double length, width, height;
+    printf("Enter Your Height: ");
+    scanf("%lf" , &height);
+    printf("Enter Your Length: ");
+    scanf("%lf" , &length);
+    printf("Enter Your Width: ");
+    scanf("%lf" , &width);
+
+
+
+    printf(" Your Volume is: %lf", height * width * length);
+
+
+    return 0;
+
 }
