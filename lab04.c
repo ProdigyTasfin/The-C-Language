76868bf498a92e8f2389070b1fa77a79c7938226
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-07-14 21:36:15 +0600

    Initial commit of my VS Code project

--- lab04.c
+++ lab04.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
 int main()
-     {
-int x = 1, i =1;
-while (i <= 9)
 {
-x = x*i;
-i = i+1;
-printf("\nThe value of x and i is %d, %d, respectively\n", x,i );
-}
-return 0;
+    //   EXERCISE 01
+    // {
+    // int x = 1, i =1;
+    // while (i <= 9)
+    // {
+    // x = x*i;
+    // i = i+1;
+    // printf("\nThe value of x and i is %d, %d, respectively\n", x,i );
+    // }
+    // EXERCISE 02
+    // int i = 0;
+    // while (i <= 99)
+    // {
+    //     scanf("%d", &i);
+    //     printf("You Enter -> %d\n", i);
+    // }
+
+    
+
+    return 0;
 }
