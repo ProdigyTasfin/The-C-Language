046f9bb032290e15c3424d200cff52bb286249d0
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-08-23 23:43:17 +0600

    Initial commit of my VS Code project

--- 2D-array_06.c
+++ 2D-array_06.c
@@ -2,15 +2,14 @@
 
 int main ()
 {
-    int i, j, a, k, b;
+    int i, j, a, b;
     printf("Number of Customers: ");
     scanf("%d",&a);
     for(i = 1; i <=a; i++){
-        for ( j = 1; j <= a; j++)
         {
             printf("\nRate the restaurant(1 to 5)\n");
             scanf("%d",&b);
-            for(k = 1; k <= b; k++){
+            for ( j = 1; j <= b; j++){
                 printf("*");
             }
         }
