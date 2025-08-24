6dc49ef5bb0df9a3dc62b5f73bfd60cc6b088afd
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-08-24 12:44:17 +0600

    Initial commit of my VS Code project

--- 2D-array_06.c
+++ 2D-array_06.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     float temp[3][4];
-    int i,j, sum, max;
-    float avg;
+    int i, j;
+    float sum, avg, max;
 
-    for (i = 0; i < 3; i++){
-        if(i == 0){
+    for (i = 0; i < 3; i++) {
+        if (i == 0)
             printf("\nEnter New York's Temperature: ");
-        }
-        else if (i == 1){
+        else if (i == 1)
             printf("\nEnter Mumbai's Temperature: ");
-        }
-        else printf("\nEnter Dhaka's Temperature: ");
+        else
+            printf("\nEnter Dhaka's Temperature: ");
 
-        for ( sum = 0, j = 0; j < 4; j++)
-        {
-            scanf("%f",&temp[i][j]);
+        sum = 0;
+
+        for (j = 0; j < 4; j++) {
+            scanf("%f", &temp[i][j]);
             sum += temp[i][j];
-            avg = sum / 4.0;
-            max = temp[0][0];
-            if(max < temp[i][j]){
-                max = temp[i][j];
-            }
+            if(j == 0)
+            max = temp[i][0];
+            else if (max < temp[i][j]){
+                max = temp[i][j];}
         }
-        printf("\nAverage temperature %.2f\n",avg);
-        printf("\nMaximum temperature %.2f\n",max);
+
+        avg = sum / 4.0;
+        printf("\nAverage temperature %.2f\n", avg);
+        printf("Maximum temperature %.2f\n", max);
     }
+
+    return 0;
 }
