4830c2f30a3c867da052a7c90d60cd60350bace7
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-08-31 22:09:44 +0600

    Initial commit of my VS Code project

--- Lab_Test_Prac_01.c
+++ Lab_Test_Prac_01.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 // If-else
+
 //Q: (Discount Calculator) A shop gives a 10% discount if the purchase amount is more than 5000. 
 // Write a C program that takes the purchase amount as input and prints the discount amount and the final bill.
 
-int main(){
+int main()
+{
      int price;
      float discount = 10.0 / 100;
      float discountedPrice;
---
+++ Lab_Test_Prac_02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//If-else
+
+//Q: (Scholarship Check) University requires students to maintain GPA above 3.00 to keep their scholarship. 
+// Write a C program to read GPA and print “Scholarship Active” if GPA ≥ 3.00, otherwise print “Scholarship Suspended”.
+
+int main()
+{
+     float gpa;
+
+    return 0;
+}
