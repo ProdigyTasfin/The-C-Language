f5414670532f0e17df666d2ea74fc2f6091593f0
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-09-01 20:41:03 +0600

    Initial commit of my VS Code project

--- Lab_Test_Prac_04.c
+++ Lab_Test_Prac_04.c
@@ -5,13 +5,19 @@
 //(Multiplication Table) Write a C program to print the multiplication tables from 1 to 10 using nested loops.
 
 int main()
-{   int i,j,a; //
+{   int i,j,a,b; //Decleration
 
-    printf("How many numbers you want to print: ");
+    printf("How many numbers you want to print: "); // Input How many numbers
     scanf("%d",&a);
 
-    for(i = 1; i <=10; i++){
-        for(j =0;j <=a; j++)
+    for(i = 1; i <=10; i++)
+    {                                   //NESTED LOOP
+        printf("Enter Your Number: ");
+        scanf("%d",&b);
+        for(j =0;j <=a; j++){
+            printf("Multiplication Table: ");
+            printf("%d x %d = %d", i,b, i*b);
+        }
     }
      
     return 0;
