96f8fc5536c86c5d96bcd90f302b138ada2f4732
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-09-01 20:31:22 +0600

    Initial commit of my VS Code project

---
+++ Lab_Test_Prac_04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//Nested-Loop
+
+//(Multiplication Table) Write a C program to print the multiplication tables from 1 to 10 using nested loops.
+
+int main()
+{   int i,j,a; //
+
+    printf("How many numbers you want to print: ");
+    scanf("%d",&a);
+
+    for(i = 1; i <=10; i++){
+        for(j =0;j <=a; j++)
+    }
+     
+    return 0;
+}
