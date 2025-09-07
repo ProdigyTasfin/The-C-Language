9566331f6d9742f2c10c3c6e76783b5152f305f4
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-09-07 23:45:18 +0600

    Initial commit of my VS Code project

---
+++ 02_Q.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{   int i,mark,n;
+    float count = 0, sum = 0, avg;
+    printf("Enter Number of Students: ");
+    scanf("%d",&n);
+    printf("Enter the marks of %d students: ",n);
+    for(i = 0; i<n; i++){
+        scanf("%d",&mark);
+        if(mark > 40){
+            count++;
+            sum +=mark;
+            avg = sum / count;
+        }
+
+    }
+    printf("The average mark (excluding those less than 40%%) is: %f",avg);
+     
+    return 0;
+}
