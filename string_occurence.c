29a905cd2c84f6cbb143638ccb8a97a8cda9955a
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-09-08 12:23:29 +0600

    Initial commit of my VS Code project

--- string_occurence.c
+++ string_occurence.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-     char c = ('u');
-     int count = 0;
-     char str[]="me";
-     for (int i = 0; i < strlen(str); i++){
-        if (str[i]== c){
+int main() {
+    char c = 'm';
+    int count = 0;
+    char str[] = "me";
+    int len = strlen(str);  // store length once
+
+    for (int i = 0; i < len; i++) {
+        if (str[i] == c) {
             count++;
         }
-     }
+    }
 
-     printf("%d", count);
+    printf("%d\n", count);
     return 0;
 }
